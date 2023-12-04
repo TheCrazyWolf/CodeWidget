4721a7d8a6db739d87fb02f3a7a4ae56498ee11e
TheCrazyWolf <tcrazywolf63@gmail.com>
2023-12-04 20:14:41 +0400

    Модель для страницы и отображения кода

---
+++ CodeVijetWeb/Models/CodeWidget.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeVijetWeb.Models;
+
+public class CodeWidget
+{
+    public string FullPath { get; set; }
+    public string ShortPath { get; set; }
+    public string Code { get; set; }
+    public bool IsCopyable { get; set; }
+}
