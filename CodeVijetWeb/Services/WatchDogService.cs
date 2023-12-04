7e5d63804ca9c3c4ec1e1fe453781290d1078ba6
TheCrazyWolf <tcrazywolf63@gmail.com>
2023-12-04 19:41:20 +0400

    Добавлены пару свойств для службы

--- CodeVijetWeb/Services/WatchDogService.cs
+++ CodeVijetWeb/Services/WatchDogService.cs
@@ -1,7 +1,22 @@
 ﻿namespace CodeVijetWeb.Services;
 
+/*
+ * Сервис отслеживающий файлы для 
+ * отображения на Виджете
+ * by TheCrazyWolf
+ */
 public class WatchDogService : BackgroundService
 {
+    /* Путь для отслеживания */
+    public string PathForTracking { get; set; } = "";
+    
+    /* Таймер для службы */
+    private readonly int _timeWait = 1000;
+    public WatchDogService()
+    {
+        
+    }
+    
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         throw new NotImplementedException();
