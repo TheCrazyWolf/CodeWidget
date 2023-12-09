8142c92597f43d0b239541aad82e284677c8ad2e
TheCrazyWolf <tcrazywolf63@gmail.com>
2023-12-09 14:27:42 +0400

    Добавил XML комментарии к моделям

--- CodeVijetWeb/Models/ListingCode.cs
+++ CodeVijetWeb/Models/ListingCode.cs
@@ -2,9 +2,24 @@
 
 public class ListingCode
 {
+    /// <summary>
+    /// Название текущего файла
+    /// </summary>
     public string FileName { get; set; }
+    /// <summary>
+    /// Полный путь файла на локальной машине (со стороны сервера)
+    /// </summary>
     public string FullPath { get; set; }
+    /// <summary>
+    /// Короткий путь к файлу \Folder\MyFile.cs
+    /// </summary>
     public string ShortPath { get; set; }
+    /// <summary>
+    /// Текст кода
+    /// </summary>
     public string Code { get; set; }
+    /// <summary>
+    /// Допускается ли копирование кода
+    /// </summary>
     public bool IsCopyable { get; set; }
 }
--- CodeVijetWeb/Models/WidgetMenu.cs
+++ CodeVijetWeb/Models/WidgetMenu.cs
@@ -2,7 +2,16 @@
 
 public class WidgetMenu
 {
+    /// <summary>
+    /// Полный путь файла на локальной машине (со стороны сервера)
+    /// </summary>
     public string FullPath { get; set; }
+    /// <summary>
+    /// Название текущего файла
+    /// </summary>
     public string FileName { get; set; }
+    /// <summary>
+    /// Короткий путь к файлу \Folder\MyFile.cs
+    /// </summary>
     public string ShortPath { get; set; }
 }
