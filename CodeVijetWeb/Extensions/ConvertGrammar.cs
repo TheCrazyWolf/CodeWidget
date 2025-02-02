a1a5d1cdc4ae7f3420f4b439e5e776790725711d
TheCrazyWolf <74318176+TheCrazyWolf@users.noreply.github.com>
2025-02-02 20:18:48 +0400

    Merge pull request #1
    * Авто определения расширения файла для подсветки синтаксиса

---
+++ CodeVijetWeb/Extensions/ConvertGrammar.cs
@@ -0,0 +1,25 @@
+﻿using PrismSharp.Core;
+
+namespace CodeVijetWeb.Extensions;
+
+public class ConvertGrammar
+{
+    public static Grammar Convert(string fileExtension)
+    {
+        switch (fileExtension.ToLower())
+        {
+            case "cs":
+                return LanguageGrammars.CSharp;
+            case "py":
+                return LanguageGrammars.Python;
+            case "css":
+                return LanguageGrammars.Css;
+            case "html":
+            case "htm":
+            case "razor":
+                return LanguageGrammars.CSHtml;
+            default:
+                return LanguageGrammars.Markup;
+        }
+    } 
+}
